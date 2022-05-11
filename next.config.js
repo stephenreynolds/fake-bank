133f2863b979ff64d0f2412dc4ef0c728c93e001
Stephen Reynolds <git@stephenreynolds.dev>
2022-05-11 11:28:49 -0400

    Add script to deploy for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProduction ? "/fake-bank/" : ""
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
