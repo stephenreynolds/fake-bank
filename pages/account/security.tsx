d9c91e0488449d4315c7fdf3b387ac72fef0bec6
Stephen Reynolds <git@stephenreynolds.dev>
2022-05-11 11:38:30 -0400

    Add inputs for security settings page

--- pages/account/security.tsx
+++ pages/account/security.tsx
@@ -1,10 +1,43 @@
+import { ChangeEvent, useState } from "react";
 import AccountSettingsLayout from "../../components/account/settings-layout";
 
 const SecuritySettings = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
     <AccountSettingsLayout>
-      <h2>Username</h2>
-      <h2>Password</h2>
+      <section>
+        <h2>Username</h2>
+        <form>
+          <label htmlFor="username">New username</label>
+          <input type="text" name="username" value={username} onChange={onUsernameChange} />
+        </form>
+      </section>
+
+      <section>
+        <h2>Password</h2>
+        <form>
+          <label htmlFor="password">New password</label>
+          <input type="password" name="password" value={password} onChange={onPasswordChange} />
+
+          <label htmlFor="confirm">New password</label>
+          <input type="password" name="confirm" value={confirmPassword} onChange={onConfirmPasswordChange} />
+        </form>
+      </section>
     </AccountSettingsLayout>
   );
 };
